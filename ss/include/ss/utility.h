0daaa1ea8de8ed21f655057a354ebe8231fd3442
lackhole <35574936+lackhole@users.noreply.github.com>
2021-07-06 03:28:25 +0900

    add forward

---
+++ ss/include/ss/utility.h
@@ -0,0 +1,25 @@
+# /*
+#  * Created by YongGyu Lee on 2021/07/06.
+#  */
+#
+# ifndef SS_UTILITY_H_
+# define SS_UTILITY_H_
+#
+# include "ss/type_traits.h"
+
+namespace ss {
+
+template<typename T> // T = U (rvalue) T = U&(lvalue) where U is no-ref
+constexpr inline T&& forward(remove_reference_t<T>& t) noexcept {
+  return static_cast<T&&>(t);
+}
+
+template<typename T>
+constexpr inline T&& forward(remove_reference_t<T>&& t) noexcept {
+  static_assert(!is_lvalue_reference<T>::value, "Forwarding rvalue to lvalue is prohibited");
+  return static_cast<T&&>(t);
+}
+
+}
+
+#endif // SS_UTILITY_H_
