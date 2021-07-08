6f397fa23d6f9c70481aa0b2b00f96068bc0de4c
lackhole <35574936+lackhole@users.noreply.github.com>
2021-07-09 00:21:08 +0900

    add test

---
+++ ss/test/type_traits/test.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#include "ss/type_traits.h"
+
+#ifdef NDEBUG
+#define NDEBUG_DEFINED 1
+#else
+#define NDEBUG_DEFINED 0
+#endif
+
+int main() {
+  std::cerr << "__cplusplus: " << __cplusplus << '\n';
+  std::cerr << "NDEBUG: " << NDEBUG_DEFINED << '\n';
+
+
+  std::cout << __cplusplus << std::endl;
+
+  return 0;
+}
