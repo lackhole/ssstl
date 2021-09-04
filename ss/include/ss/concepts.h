67911a859aac659194351a9ad0516cb6791af831
Tony <cosgenio@gmail.com>
2021-09-04 22:00:58 +0900

    Create concepts.h

---
+++ ss/include/ss/concepts.h
@@ -0,0 +1,14 @@
+# /*
+#  * Created by YongGyu Lee on 2021/07/10.
+#  */
+#
+# ifndef SS_CONCEPTS_H_
+# define SS_CONCEPTS_H_
+#
+# include "ss/type_traits.h"
+
+namespace ss {
+
+}
+
+# endif // SS_CONCEPTS_H_
