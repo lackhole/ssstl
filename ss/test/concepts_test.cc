eba599e6e7f7955443514e2d1417a9e27fb8c27a
Tony <cosgenio@gmail.com>
2021-09-04 21:56:41 +0900

    Create concepts_test.cc

---
+++ ss/test/concepts_test.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <utility>
+#include <string>
+
+//#include "ss/concepts.h"
+
+#include "ss_test.h"
+#include "test_utils.h"
+
+#ifdef NDEBUG
+#define NDEBUG_DEFINED 1
+#else
+#define NDEBUG_DEFINED 0
+#endif
+
+
+
+int main() {
+  SS_INIT_TEST("utility")
+  std::cerr << "__cplusplus: " << __cplusplus << '\n';
+  std::cerr << "NDEBUG: " << NDEBUG_DEFINED << '\n';
+
+  SS_TEST_RETURN
+}
