aa2f9348e5822cd9685273de266312f62781b76f
Tony <cosgenio@gmail.com>
2021-11-02 00:49:54 +0900

    add max

--- ss/include/ss/algorithm.h
+++ ss/include/ss/algorithm.h
@@ -5,10 +5,19 @@
 # ifndef SS_ALGORITHM_H_
 # define SS_ALGORITHM_H_
 #
-# include "ss/detail/swap.h"
+# include "ss/detail/macro.h"
 
 namespace ss {
 
+template<typename T>
+SS_CONSTEXPR_AFTER_14 const T& max(const T& a, const T& b) {
+  return ((a > b)) ? a : b;
+}
+
+template<typename T, typename Compare>
+SS_CONSTEXPR_AFTER_14 const T& max(const T& a, const T& b, Compare comp) {
+  return (comp(a, b)) ? a : b;
+}
 
 } // namespace ss
 
