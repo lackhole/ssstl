ce43a68a0b98aff3418fd0dabec970e4125668d1
Tony <cosgenio@gmail.com>
2022-01-21 03:19:27 +0900

    Add vector

---
+++ ss/include/ss/vector.h
@@ -0,0 +1,11 @@
+# /*
+#  * Created by YongGyu Lee on 2022/01/21.
+#  */
+#
+# ifndef SS_VECTOR_H_
+# define SS_VECTOR_H_
+# 
+# include "ss/initializer_list.h"
+# 
+
+# endif // SS_VECTOR_H_
---
+++ test/vector_test.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <utility>
+#include <string>
+
+#include "ss/vector.h"
+
+#include "ss_test.h"
+
+#ifdef NDEBUG
+#define NDEBUG_DEFINED 1
+#else
+#define NDEBUG_DEFINED 0
+#endif
+
+
+
+int main() {
+  SS_INIT_TEST("vector")
+  std::cerr << "__cplusplus: " << __cplusplus << '\n';
+  std::cerr << "NDEBUG: " << NDEBUG_DEFINED << '\n';
+
+
+  SS_TEST_RETURN
+}
