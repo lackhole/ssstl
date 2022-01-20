1017787db91a21345f90ccb521d893059b9a5c48
Tony <cosgenio@gmail.com>
2022-01-21 02:47:05 +0900

    Create iterator_test

---
+++ test/iterator_test.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <utility>
+#include <string>
+
+#include "ss/iterator.h"
+
+#include "ss_test.h"
+
+#ifdef NDEBUG
+#define NDEBUG_DEFINED 1
+#else
+#define NDEBUG_DEFINED 0
+#endif
+
+
+
+int main() {
+  SS_INIT_TEST("iterator")
+  std::cerr << "__cplusplus: " << __cplusplus << '\n';
+  std::cerr << "NDEBUG: " << NDEBUG_DEFINED << '\n';
+
+  SS_TEST_RETURN
+}
